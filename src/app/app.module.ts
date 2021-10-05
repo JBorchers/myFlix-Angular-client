425d6043ffeea4af7b9499f18851c78c2b805782
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-10-04 19:15:40 -0600

    added navbar and movie synopsis to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,8 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { MovieDirectorComponent } from './movie-director/movie-director.component';
 import { MovieGenreComponent } from './movie-genre/movie-genre.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MovieSynopsisComponent } from './movie-synopsis/movie-synopsis.component';
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
@@ -44,7 +46,9 @@ const appRoutes: Routes = [
     WelcomePageComponent,
     MovieDirectorComponent,
     MovieGenreComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    NavBarComponent,
+    MovieSynopsisComponent
   ],
   imports: [
     BrowserModule,
